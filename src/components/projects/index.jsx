c1d0350fd6b7861ebe4186d4692d2e37047ecad8
Vikram Kangotra <vikramkangotra8055@gmail.com>
2023-07-27 18:17:42 +0530

    fix

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -9,14 +9,14 @@ export default function Projects() {
     return (
         <div className="flex flex-col items-center justify-start p-20 overflow-hidden">
             <div className="flex flex-col items-start content-start justify-start max-w-4xl overflow-visible gap-5 min-h-max">
-                <p className="text-base font-medium opacity-60">Projects</p>
-                <div className="grid grid-cols-2 grid-rows-2 gap-5">
+                <p className="text-lg font-medium opacity-60">Projects</p>
+                <div className="grid grid-cols-2 grid-rows-2 gap-2 md:gap-5">
                     <ProjectCard href={projects[0].href} imageSrc={projects[0].imageSrc} projectName={projects[0].name} />
                     <ProjectCard href={projects[1].href} imageSrc={projects[1].imageSrc} projectName={projects[1].name} />
                     <ProjectCard href={projects[2].href} imageSrc={projects[2].imageSrc} projectName={projects[2].name} />
                     <ProjectCard href={projects[3].href} imageSrc={projects[3].imageSrc} projectName={projects[3].name} />
                 </div>
-                <p className="w-full text-right text-gray-500 dark:text-gray-400">
+                <p className="w-full text-base text-right text-gray-500 dark:text-gray-400">
                     More projects are waiting to be explored.
                     <Link href="#" className="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline">
                         Explore all projects
--- src/components/projects/project-card.jsx
+++ src/components/projects/project-card.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function ProjectCard({href, imageSrc, projectName}) {
     return (
         <Link href={href}>
-            <div className='relative overflow-hidden group rounded-3xl grayscale hover:grayscale-0 '>
+            <div className='relative overflow-hidden rounded-xl group md:rounded-3xl grayscale hover:grayscale-0 '>
                 <span className={`absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 translate-y-full bg-black group-hover:translate-y-16 ease opacity-20 blur-2xl`}>
                 </span>
                 <span className={`absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 translate-y-full group-hover:translate-y-0 ease text-4xl text-left text-bold`}>
