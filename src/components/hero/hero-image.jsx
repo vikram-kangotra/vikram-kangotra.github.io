79813a07bfa4e7791134e6c7d10db66a857acde3
Vikram Kangotra <vikramkangotra8055@gmail.com>
2023-08-06 23:30:43 +0530

    fix

--- src/components/hero/hero-image.jsx
+++ src/components/hero/hero-image.jsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function HeroImage({src, alt, containerClassName, imageClassName}) {
     return (
         <div className={`aspect-square flex-wrap flex justify-center items-center p-3 box-sizing w-60 h-60 rounded-xl overflow-hidden flex-shrink-0 ${containerClassName}`}>
-            <Image className={`${imageClassName}`} src={src} alt={alt} />
+            <Image className={`${imageClassName}`} src={src} alt={alt} priority={true} />
         </div>
     )
 }
