03b5b3505194c5d06e9f8e7e76dfe36bb5534d9c
Vikram Kangotra <vikramkangotra8055@gmail.com>
2023-08-20 17:34:18 +0530

    added google site verification

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="google-site-verification" content="mfXmatB1-JrCwUSXz5TVVuR5zK5cn0N9-QzpM87jtgs" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.4.0/styles/github-dark.min.css" />
       </Head>
       <body>
