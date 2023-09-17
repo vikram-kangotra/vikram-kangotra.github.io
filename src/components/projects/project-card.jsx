4247774428ae44aeb2c9cb260831b62ce5de52d6
Vikram Kangotra <vikramkangotra8055@gmail.com>
2023-09-18 00:38:43 +0530

    Updated for mobile

--- src/components/projects/project-card.jsx
+++ src/components/projects/project-card.jsx
@@ -8,7 +8,7 @@ export default function ProjectCard({href, imageSrc, projectName}) {
                 <span className='absolute inset-0 opacity-50 bg-slate-950 md:translate-y-0 translate-y-full group-hover:translate-y-full'/>
                 <span className={`absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 md:translate-y-full bg-slate-950 group-hover:translate-y-16 ease opacity-20 blur-2xl`}>
                 </span>
-                <span className={`absolute inset-0 flex items-center justify-center w-full h-full md:text-white duration-300 translate-y-5 md:translate-y-full md:group-hover:translate-y-0 ease text-lg md:text-4xl text-bold`}>
+                <span className={`absolute inset-0 flex items-center justify-center w-full h-full md:text-white duration-300 translate-y-5 md:translate-y-full md:group-hover:translate-y-0 ease text-2xl md:text-4xl text-bold`}>
                     {projectName}
                 </span>
                 <Image src={imageSrc} alt="project image" />
