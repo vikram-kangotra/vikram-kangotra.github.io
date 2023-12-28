5baf095e975fda73cccb48b0462f2ff20bb4c20f
Vikram Kangotra <vikramkangotra8055@gmail.com>
2023-12-29 01:57:30 +0530

    Changed where I'm found mostly

--- src/components/introduction/index.jsx
+++ src/components/introduction/index.jsx
@@ -14,7 +14,7 @@ export default function Introduction() {
 Step into my realm, where the worlds of coding and Linux converge in captivating harmony.</p>
                         <p className="py-3 italic font-medium tracking-wide text-white text-md md:text-lg opacity-90">
                             I&apos;m a software developer fueled by a love for coding and Linux. 
-                            <br/>Every now and then I <del className="px-1 text-white line-through bg-red-600"> break</del> <ins className="px-1 text-white bg-blue-600 bg-blue-800"> tinker</ins> my linux machine. <br/> Sharing experiences and insights through writing.
+                            <br/>Every now and then I <del className="px-1 text-white line-through bg-red-600"> break</del> <ins className="px-1 text-white bg-blue-800"> tinker</ins> my linux machine. <br/> Sharing experiences and insights through writing.
                         </p>
                     </div>
                     <figcaption className="font-medium">
@@ -22,7 +22,7 @@ Step into my realm, where the worlds of coding and Linux converge in captivating
                             Vikram Kangotra
                         </div>
                         <div>
-                            Student at <Link href="https://nitsri.ac.in/" className="hover:underline">NIT Srinagar</Link>
+                            Spotted at the <Link href="https://github.com/vikram-kangotra"><ins>coding desk</ins></Link>
                         </div>
                     </figcaption>
                 </div>
