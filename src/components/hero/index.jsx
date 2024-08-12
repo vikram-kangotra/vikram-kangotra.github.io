7768398ad0cc722987b723a1426e641bcaa910e9
Vikram Kangotra <vikramkangotra8055@gmail.com>
2024-08-13 04:51:27 +0530

    fix: overfloow

--- src/components/hero/index.jsx
+++ src/components/hero/index.jsx
@@ -6,19 +6,19 @@ import Logo from "../logo"
 export default function Hero() {
 
   return (
-      <header className='flex flex-col items-center content-center justify-start py-10 overflow-hidden md:py-20 gap-16 md:gap-28'>
+      <header className='flex flex-col items-center content-center justify-start py-10 md:py-20 gap-16 md:gap-28'>
         
         <Logo />
 
         <div className="relative">
 
-          <div className="overflow-hidden absolute bg-yellow-300 rounded-full right-0 top-10 blur-3xl w-72 h-72 opacity-20"></div>
+          <div className="absolute right-0 bg-yellow-300 rounded-full blur-3xl w-72 h-72 opacity-20"></div>
 
           <div className="flex flex-col items-center justify-center px-5">
 
             <h1 className={`text-4xl font-bold text-center md:text-7xl`}><Typewriter text="Code. Passion. Linux. " delay={100}></Typewriter></h1>
             <p className="relative max-w-3xl my-5 text-sm text-center opacity-90 md:text-lg text-tertiary leading-7">
-Discover the captivating universe of Vikram Kangotra, a passionate programming enthusiast on a continuous learning odyssey. Explore intricate code creations driven by Linux and open-source harmony, unveiling a symphony of unity. Immerse yourself in innovative projects, enlightening insights, and the evolving artistry of software development. Join me on this thrilling tech journey, crafting our digital masterpiece together, keystroke by keystroke.
+                Explore my world as a passionate programmer always eager to learn and code. I’m diving into various projects and sharing my journey through software development and innovation. Follow along as I build and create, one keystroke at a time!
             </p>
             <div className="flex flex-col justify-center md:flex-row gap-3">
               <HeroButton link="projects" bgColor="slate-800" textColor="tertiary">View Projects</HeroButton>
--- src/components/introduction/index.jsx
+++ src/components/introduction/index.jsx
@@ -2,16 +2,16 @@ import Link from "next/link"
 
 export default function Introduction() {
     return (
-        <div className="flex flex-col items-center justify-start p-10 overflow-hidden">
+        <div className="flex flex-col items-center justify-start p-10">
             <div className="relative flex flex-col items-start content-start justify-start max-w-4xl">
 
-                <div className="overflow-hidden absolute bg-pink-300 rounded-full left-24 top-8 opacity-20 blur-3xl w-64 h-64"></div>
+                <div className="absolute w-64 h-64 bg-pink-300 rounded-full left-24 top-8 opacity-20 blur-3xl"></div>
 
                 <div className="pt-6 my-auto text-center space-y-4">
                     <div>
                         <p className="mb-3 text-xl font-semibold tracking-wide text-gray-200 md:text-3xl">
-                            Greetings, fellow tech explorer!<br/>
-Step into my realm, where the worlds of coding and Linux converge in captivating harmony.</p>
+                            Greetings, fellow tech enthusiast!<br/>
+Step into my world, where coding and innovation come together in exciting ways.</p>
                         <p className="py-3 italic font-medium tracking-wide text-white text-md md:text-lg opacity-90">
                             I&apos;m a software developer fueled by a love for coding and Linux. 
                             <br/>Every now and then I <del className="px-1 text-white line-through bg-red-600"> break</del> <ins className="px-1 text-white bg-blue-800"> tinker</ins> my linux machine. <br/> Sharing experiences and insights through writing.
