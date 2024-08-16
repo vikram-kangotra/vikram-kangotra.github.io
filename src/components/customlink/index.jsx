00b9ad7f728f464bb54e3c21ef6574283f567e28
Vikram Kangotra <vikramkangotra8055@gmail.com>
2024-08-17 02:14:21 +0530

    started working on technical details: interface & extension

--- src/components/customlink/index.jsx
+++ src/components/customlink/index.jsx
@@ -4,7 +4,7 @@ const CustomLink = ({href, children}) => {
     return (
         <Link 
             href={href} 
-            className="text-teal-400 md:no-underline hover:text-teal-300 transition-colors duration-300 hover:underline"
+            className="text-teal-400 hover:text-teal-300 transition-colors duration-300 hover:underline"
         >
             {children}
         </Link>
